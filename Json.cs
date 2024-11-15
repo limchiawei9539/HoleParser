c97ce7afce1805c64ff07dcafcce85d230c78294
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2024-11-15 16:21:24 +0800

    Update Json.cs

--- Json.cs
+++ Json.cs
@@ -9,29 +9,5 @@ namespace HoleParser
     public class MyJsonData
     {
         public string Holes { get; set; }
-        public List<string> charLocation { get; set; }
-        public List<string> villagerLevels { get; set; }
-        public List<string> villagerExp { get; set; }
-        public List<string> opalsInvested { get; set; }
-        public List<string> holeMajik { get; set; }
-        public List<string> villageMajik { get; set; }
-        public List<string> idleonMajik { get; set; }
-        public List<string> opalsFound { get; set; }
-        public List<string> List9 { get; set; }
-        public List<string> List10 { get; set; }
-        public List<string> List11 { get; set; }
-        public List<string> List12 { get; set; }
-        public List<string> List13 { get; set; }
-        public List<string> List14 { get; set; }
-        public List<string> List15 { get; set; }
-        public List<string> List16 { get; set; }
-        public List<string> List17 { get; set; }
-        public List<string> List18 { get; set; }
-        public List<string> List19 { get; set; }
-        public List<string> List20 { get; set; }
-        public List<string> List21 { get; set; }
-        public List<string> List22 { get; set; }
-        public List<string> List23 { get; set; }
-        public List<string> List24 { get; set; }
     }
 }
